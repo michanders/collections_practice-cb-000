c6b1e20c72d4d0c23b13150487df8654fe1ff3b3
Michael Anderson <michanders@msn.com>
2017-09-28 22:51:20 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,60 @@
 
+
+def sort_array_asc(array)
+  array.collect.sort
+end
+
+def sort_array_desc(array)
+  array.collect.sort do |a,b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.each.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(array)
+  return array.reverse
+end
+
+
+def kesha_maker(array)
+  array.each do |string|
+    string[2] = "$"
+  end
+end
+
+
+def find_a(array)
+  array.select do |strings|
+    strings.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  sum = 0
+  i = 0
+  while i < array.length
+    sum = sum + array[i]
+    i += 1
+  end
+  return sum
+end
+
+def add_s(array)
+  array.each_with_index.collect do |word, index|
+    if index != 1
+      "#{word}s"
+    else
+      "#{word}"
+    end
+  end
+end
